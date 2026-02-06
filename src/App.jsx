76207a1f5366eb6a9fd1a6ef57c713cbd3d60ca1
[paswanayush2303-pixel] <[paswanayush2303@gmail.com]>
2026-02-06 18:19:06 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
 
 function App() {
   return (
-    <BrowserRouter basename="/Movie-explorer/">
+    <HashRouter basename="/Movie-explorer/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
